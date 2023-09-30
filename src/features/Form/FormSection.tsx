14ef8eedefafcf24c36a2c24b2865f086d738258
Kacper Machaj <kacpermachay@gmail.com>
2023-09-30 21:14:38 +0200

    remove console logs

--- src/features/Form/FormSection.tsx
+++ src/features/Form/FormSection.tsx
@@ -1,17 +1,12 @@
 import { Button, Form } from "antd";
 import { Region } from "./Region";
 import { UniversityType } from "./UniversityType";
-import { useRegionOptions } from "./utils/useGetRegionOptions.hook";
 
 export const FormSection: React.FC = () => {
   const [form] = Form.useForm();
   const handleSubmit = (values: any) => {
     console.log(values);
   };
-
-  const regionOption = useRegionOptions();
-
-  console.log(regionOption);
 
   return (
     <Form form={form} onFinish={handleSubmit} name="form">
